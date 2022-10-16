2deaf7d067b93fe12be11d1e6f919b7de11033cf
小企鹅 <2557173038@qq.com>
2022-10-16 17:33:35 +0800

    项目-05-组件库的配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/elementUI' // 让elementUI参与打包，才能在页面上注册插件
+import '@/assets/global.less'
 
 Vue.config.productionTip = false
 
