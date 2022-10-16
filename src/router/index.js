a9b31c921abd51417bf35c457c52a5de2b711c2e
小企鹅 <2557173038@qq.com>
2022-10-16 21:47:43 +0800

    项目-07-登录和注册页面的准备

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,19 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/reg',
+    component: () => import('@/views/register/index')
+    // webpack提供的import函数来路由懒加载导入组件
+    // 路由懒加载，就是页面路由路径切换到、reg，采取加载对应组件代码
+    // 好处：让首页加载文件体积更小，加载更快
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+
+  }
+
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>login.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'my-login'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
