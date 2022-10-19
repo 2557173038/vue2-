638bb156a17f5f4286d7f7774b58e0cc59b5184e
小企鹅 <2557173038@qq.com>
2022-10-19 22:42:15 +0800

    14-利用 vuex-persistedstate 插件实现vuex持久化存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -18,5 +18,12 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  // 配置vuex的插件
+  plugins: [
+    createPersistedState() // 调用函数 注入持久化插件
+  ]
 })
+
+// vuex默认保存在内存中，所以刷新所有的值会回归初始化（无法做到持久化储存）
+// 借助yarn add vuex-persistedstate@3.2.1这个包可以让vuex做持久化存储
