9600fa528aeae3490989be62938a047f2d28ecce
小企鹅 <2557173038@qq.com>
2022-10-22 22:01:33 +0800

    项母26-axios响应拦截器-判断401状态

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,8 @@
 // 基于axios封装，网络请求函数
 import axios from 'axios'
 import store from '@/store'
+import router from '@/router'
+import { Message } from 'element-ui'
 // import store from 'vuex'
 // axios create()创建一个带配置项的自定义axios函数
 // myAxios请求的时候，地址baseURL+url(url拼接在后面),然后去请求后台
@@ -34,4 +36,26 @@ myAxios.interceptors.request.use(function (config) {
   //   reject(error)
   // }
 })
+
+// 定义响应拦截器
+myAxios.interceptors.response.use(function (response) {
+  // 响应http状态码为 2xx或3xx 时触发成功的回调，形参中的 response 是“成功的结果”
+  // return到axios原地promise对象，作为成功的结果
+  return response
+}, function (error) {
+  // console.dir(error)
+  // 响应状态码为 4xx或5xx 时触发失败的回调，形参中的 error 是“失败的结果”
+  // return到axios原地promise对象位置，作为失败拒绝的状态(如果那边用try+catch或者catch函数捕获，可以捕获到我们传递过去的这个error变量的值)
+  if (error.response.status === 401) {
+    // 本次响应是token过期了
+    // 清除vuex里的一切，然后切换到登录页面（被动退出登录）
+    store.commit('updateToken', '')
+    store.commit('updateUserInfo', {})
+    router.push('/login')
+    Message.error('用户身份已过期')
+  }
+  return Promise.reject(error)
+})
+
+// 导出axios自定义函数
 export default myAxios
