6910c315c23af6292cf103d18980426c66f01716
小企鹅 <2557173038@qq.com>
2022-11-01 23:08:01 +0800

    53-发布文章-封面文章的准备和保存图片

--- src/views/Art/artList.vue
+++ src/views/Art/artList.vue
@@ -14,7 +14,12 @@
               placeholder="请选择分类"
               size="small"
             >
-              <el-option v-for="data in this.cateList" :label="data.cate_name" :value="data.id" :key="data.id"></el-option>
+              <el-option
+                v-for="data in this.cateList"
+                :label="data.cate_name"
+                :value="data.id"
+                :key="data.id"
+              ></el-option>
             </el-select>
           </el-form-item>
           <el-form-item label="发布状态" style="margin-left: 15px">
@@ -65,12 +70,38 @@
             placeholder="请选择分类"
             style="width: 100%"
           >
-          <!-- 因为整个标单要发给后台，去提前看眼vue代码里绑定是值需要什么，发现接口文档要的是分类id -->
-            <el-option v-for="data in this.cateList" :label="data.cate_name" :value="data.id" :key="data.id"></el-option>
+            <!-- 因为整个标单要发给后台，去提前看眼vue代码里绑定是值需要什么，发现接口文档要的是分类id -->
+            <el-option
+              v-for="data in this.cateList"
+              :label="data.cate_name"
+              :value="data.id"
+              :key="data.id"
+            ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item  lable="文章内容" prop="content">
-            <quill-editor v-model="pubForm.content"></quill-editor>
+        <el-form-item lable="文章内容" prop="content">
+          <quill-editor v-model="pubForm.content"></quill-editor>
+        </el-form-item>
+        <!-- 文章封面 -->
+        <el-form-item label="文章封面">
+          <!-- 用来显示封面的图片 -->
+          <img
+            src="../../assets/images/cover.jpg"
+            alt=""
+            class="cover-img"
+            ref="imgRef"
+          />
+          <br />
+          <!-- 文件选择框，默认被隐藏 -->
+          <input
+            type="file"
+            style="display: none"
+            accept="image/*"
+            ref="iptFileRef"
+            @change="changeCoverFn"
+          />
+          <!-- 选择封面的按钮 -->
+          <el-button type="text" @click="selCoverFn">+ 选择封面</el-button>
         </el-form-item>
       </el-form>
     </el-dialog>
@@ -94,8 +125,8 @@ export default {
         // 发布文章-表单的数据对象
         title: '', // 文章标题
         cate_id: '', // 文章分类id
-        content: '' // 发布内容
-
+        content: '', // 发布内容
+        cover_img: '' // 封面图片(保存的是文件)
       },
       pubFormRules: {
         // 发布文章-表单的验证规则对象
@@ -107,7 +138,6 @@ export default {
             message: '文章标题的长度为1-30个字符',
             trigger: 'blur'
           }
-
         ],
         cate_id: [
           { required: true, message: '请选择文章标题', trigger: 'blur' }
@@ -164,6 +194,23 @@ export default {
     async getCateListFn () {
       const { data: res } = await updateArtCateListAPI()
       this.cateList = res.data
+    },
+    // 选择封面点击事件->让文件选择窗口出现
+    selCoverFn () {
+      this.$refs.iptFileRef.click() // 用js代码模拟一次点击事件动作
+    },
+    // 用户选择了封面文件
+    changeCoverFn (e) {
+      // e.target 拿到触发事件的那个标签(input标签对象本身)
+      // e.target.files 拿到选择的文件数组
+      const files = e.target.files
+      if (files.length === 0) {
+        // 用户没有选择图片 把cover_img置空
+        this.pubForm.cover_img = ''
+      } else {
+        // 用户选择了图片，把文件直接保存到表单对象的属性里
+        this.pubForm.cover_img = files[0]
+      }
     }
   }
 }
@@ -185,5 +232,10 @@ export default {
 ::v-deep .ql-editor {
   min-height: 300px;
 }
-
+// 设置图片封面的宽高
+.cover-img {
+  width: 400px;
+  height: 280px;
+  object-fit: cover;
+}
 </style>
