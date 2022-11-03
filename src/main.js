d09ea9e38dbfc3b7049719d9665cf44973bf9764
小企鹅 <2557173038@qq.com>
2022-11-03 22:48:48 +0800

    64-文章列表格式化发布的时间

--- src/main.js
+++ src/main.js
@@ -11,6 +11,14 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+
+import dayjs from 'dayjs'
+// 基于dayjs封装一个全局函数来格式化事件(任意的组件可以直接使用$formatDate)
+Vue.prototype.$formatDate = (dateObj) => {
+  // 借助dayjs内置的方法把日期对象格式化指定的格式，并把格式化好的字符串返回到调用处
+  return dayjs(dateObj).format('YYYY-MM-DD HH:mm:ss')
+}
+
 Vue.config.productionTip = false
 
 Vue.use(VueQuillEditor) // 此方法内会用vue，component注册quilleditor名字全局组件
