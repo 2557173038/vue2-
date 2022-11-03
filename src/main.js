5050e2acb1a5168421dd5b1dfcd73fc60e31c526
小企鹅 <2557173038@qq.com>
2022-11-03 20:50:06 +0800

    62-前端预览图片格式和发给后台图片格式总结

--- src/main.js
+++ src/main.js
@@ -20,3 +20,16 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// 前端：
+// 前提：通过input[type=file]让用户选择文件，通过事件对象.target.files拿到用户选择的‘文件对象’
+// 预览：img标签的src属性的值(baes64字符串 / 图片链接地址)
+//    情况1:给src属性赋予baes64字符串(图片数据转base64字符串)，特点以data:image/png;base64，开头
+//    文件转成图片base64字符串：new FileReader()
+
+//    情况2：文件转成图片链接地址: URL.createObjURL(文件)
+//    注意：他的地址只能在前端使用
+
+// 需求：把用户选择的文件发给后台保存在服务器上
+//      情况1：文件转成baes64字符串，传递给后套后台
+//      情况2：用new FromData()表单数据直接装文件本身，直接传递给后台
