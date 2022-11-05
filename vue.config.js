1259cd04694878b3c070e473380f57dfca4011bb
小企鹅 <2557173038@qq.com>
2022-11-05 22:01:01 +0800

    76-打包发布——publicPath的值修改

--- vue.config.js
+++ vue.config.js
@@ -14,5 +14,15 @@ module.exports = defineConfig({
     }
   },
   transpileDependencies: true,
-  publicPath: './'
+  // ./可以让开发环境和生产环境都能正常使用
+  // 为了严谨一点
+  // 开发环境: '/'
+  // 生产环境: './'
+  // 问题:有无代码可以让他自己识别当前运行环境
+  // node里有个内置的环境变量process.eav.NODE_ENV
+  // rocess.eav.NODE_ENV 他会根据你敲击的命令，来使用不同的值
+  // 解决：
+  // 如果你敲击的是yarn serve rocess.env.NODE_ENV的值就是development字符串
+  // 如果你敲击的是yarn build rocess.env.NODE_ENV的值就是production字符串
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 })
